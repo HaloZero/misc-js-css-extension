1c70ca4098e466390de24b7ff966687e5b9c47e1
rohan <halo.zero00@gmail.com>
2015-03-04 09:35:41 -0800

    Fixing issues with display and trying to get rid of jewel count

--- facebook/facebook.js
+++ facebook/facebook.js
@@ -1,3 +1,8 @@
-$(".uiSideNav .count.uiSideNavCount").hide();
+function mainLoop() {
+  $(".uiSideNav .count.uiSideNavCount").hide();
+  // changes from 09-26-2014
+  $(".homeSideNav .countValue").hide()
+  $("a[title=Events]").attr('href','/events/calendar');
+}
 
-$("a[title=Events]").attr('href','/events/calendar');
+setInterval(mainLoop, 1000);
