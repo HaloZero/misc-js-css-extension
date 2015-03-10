90ae869cc4dee7270fbf818851e501d170650bce
rohan <halo.zero00@gmail.com>
2015-03-10 00:09:06 -0700

    CSS hiding notification, stopping title from changing

--- facebook/facebook.js
+++ facebook/facebook.js
@@ -3,6 +3,8 @@ function mainLoop() {
   // changes from 09-26-2014
   $(".homeSideNav .countValue").hide()
   $("a[title=Events]").attr('href','/events/calendar');
+  $('title').text('Facebook');
 }
 
 setInterval(mainLoop, 1000);
+
