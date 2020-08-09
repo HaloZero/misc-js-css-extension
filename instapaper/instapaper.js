b0d5cacd6b30549dd0a49674ff325ac43a58d988
rohan <rohan.dhaimade@gmail.com>
2020-08-09 10:06:07 -0700

    Moving to not using jquery

--- instapaper/instapaper.js
+++ instapaper/instapaper.js
@@ -1,22 +1,18 @@
 $(function() {
-
-  var $articleList = $("#article_list");
-  var $articles = $articleList.find(".article_item");
-  for (var i = 0; i < $articles.length; i++) {
-    var $article = $($articles[i]);
-    var sourceURL = $article.find(".title_meta a").attr("href");
-    $article.find(".title_row a").attr("href", sourceURL);
+  var articles = document.querySelectorAll("#article_list .article_item")
+  for (var i = 0; i < articles.length; i++) {
+    var article = articles[i];
+    var sourceURL = article.querySelectorAll(".title_meta a")[0].getAttribute("href")
+    article.querySelectorAll(".title_row a")[0].setAttribute("href", sourceURL);
   }
 
-  // Override for some reason the max-width property on css stops this from appearing.
-  $('.article_inner_item').hover(
-    function() {
-      $(this).find('.article_item_footer .article_actions').show()
-      $(this).find('.article_item_footer .article_actions .action_link').css('opacity', 1)
-    },
-    function() {
-      $(this).find('.article_item_footer .article_actions').hide()
-      $(this).find('.article_item_footer .article_actions .action_link').css('opacity', 0)
-    }
-  );
+  document.querySelector(".article_inner_item").addEventListener("mouseover", event => {
+    event.target.querySelector('.article_item_footer .article_actions').style.display = 'block'
+    event.target.querySelector('.article_item_footer .article_actions .action_link').style.opacity = 1
+  });
+
+  document.querySelector(".article_inner_item").addEventListener("mouseleave", event => {
+    event.target.querySelector('.article_item_footer .article_actions').style.display = 'hidden'
+    event.target.querySelector('.article_item_footer .article_actions .action_link').style.opacity = 0
+  });
 });
