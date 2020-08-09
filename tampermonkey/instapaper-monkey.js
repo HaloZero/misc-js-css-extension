eb9bec57d9309c73b3949757922cdcca78097d60
rohan <rohan.dhaimade@gmail.com>
2020-08-09 10:16:13 -0700

    Adding tampemonkey scripts

---
+++ tampermonkey/instapaper-monkey.js
@@ -0,0 +1,9 @@
+// ==UserScript==
+// @name         Instapaper
+// @namespace    instapaper
+// @version      0.2
+// @description  try to take over the world!
+// @author       You
+// @match        *://*.instapaper.com/*
+// @grant        none
+// ==/UserScript==
---
+++ tampermonkey/podcast-monkey.js
@@ -0,0 +1,20 @@
+// ==UserScript==
+// @name         Podcast
+// @namespace    Podcasting
+// @version      0.1
+// @description  Podcast script
+// @author       You
+// @match        *://*.thisamericanlife.org/*
+// @match        *://*.gimletmedia.com/*
+// @match        *://*.stuffyoushouldknow.com/*
+// @match        *://*.99percentinvisible.org/*
+// @match        *://*.thisiscriminal.com/*
+// @match        *://*.loveandradio.org/*
+// @match        *://*.lorepodcast.com/*
+// @match        *://*.radiolab.org/*
+// @match        *://*.imaginaryworldspodcast.org/*
+// @match        *://*.wnycstudios.org/*
+// @match        *://*.iheart.com/*
+// @require      http://code.jquery.com/jquery-latest.js
+
+// ==/UserScript==
